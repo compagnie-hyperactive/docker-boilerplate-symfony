ca719ab3ecf9c95cb8c04549eda73f1546812db2
ML <mleorat@compagnie-hyperactive.com>
2017-11-20 09:02:51 +0100

    Fix xDebug Conf + change default HOST_USER value

---
+++ symfony/src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthieu
+ * Date: 19/11/17
+ * Time: 18:21
+ */
+
+namespace App\Controller;
+
+
+class DefaultController
+{
+
+}
---
+++ symfony/src/Entity/Document.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthieu
+ * Date: 19/11/17
+ * Time: 19:38
+ */
+
+namespace App\Entity;
+
+
+class Document
+{
+
+}
